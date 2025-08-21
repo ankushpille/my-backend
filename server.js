df4c6793eb97fe104c726f671fc800a17b586a4b
ankushpille <ankush.pille1@gmail.com>
2025-08-21 22:17:58 +0530

    implemented todos get api post api

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
+
+
+const todos = [
+    {id:1,task:"learn nodejs",completed:false},
+    {id:2,task:"learn express",completed:false},
+    {id:3,task:"learn mongodb",completed:false}
+]
+
+app.get('/todos',(req,res) => {
+    res.json(todos);
+})
+
+app.post('/todos',(req,res) => {
+    const newTodo = {
+        id: todos.length +1,
+        task:req.body.task,
+        completed:false
+    }
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+})
+
+app.listen(3000,() => {
+    console.log("server running on port 3000")
+})
